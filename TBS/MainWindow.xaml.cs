303eed9aa75726b4bed73fd9c383b56cb9533d65
Antanas <sepikas.antanas@gmail.com>
2015-01-01 18:41:05 +0200

    first commit

---
+++ TBS/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Controls.Ribbon;
+using HtmlAgilityPack;
+
+namespace TBS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string PollAdress = "http://topsport.betgames.tv/ext/game/results/topsport/";
+        private int PollTimeout = 5000;
+        private string TableId = "table";
+
+        private Poller Poller;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            Poller = new Poller(PollAdress, TableId, PollTimeout);
+            
+            this.DataContext = Poller;
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void TBS_ContentRendered(object sender, EventArgs e)
+        {
+            StatusState.Content = "Waiting...";
+            Progress.Value = 100;
+
+            
+
+            StatusState.Content = "Waiting...";
+            Progress.Value = 0;
+        }
+
+        private void Test_Click(object sender, RoutedEventArgs e)
+        {
+            Poller.RefreshList();
+            this.DataContext = null;
+            this.DataContext = Poller;
+            MessageBox.Show("Updated");
+        }
+
+    }
+}
---
+++ TBS/Poller.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace TBS
+{
+    class Poller
+    {
+        private string PollAdress;
+        private string TableId;
+        private int PollTimeout;
+
+        public List<string> RollList { get; set; }
+
+        public Poller(string PollAdress, string TableId, int PollTimeout = 1000)
+        {
+            this.PollAdress = PollAdress;
+            this.TableId = TableId;
+            this.PollTimeout = PollTimeout;
+
+            RefreshList(false);
+        }
+
+        public void Start()
+        {
+            //start thread
+        }
+
+        public void Stop()
+        {
+            //kill thread
+        }
+
+        public void RefreshList(bool first = true)
+        {
+            List<string> TmpList = new List<string>();
+
+            if (RollList != null)
+            {
+                TmpList = RollList;
+            }
+
+            foreach (List<int> Item in GetData(first))
+            {
+                if (first == false) {
+                    TmpList.Add(String.Join(" ", Item.ToArray()));
+                }
+                else
+                {
+                    TmpList.Insert(0, String.Join(" ", Item.ToArray()));
+                }
+                
+            }
+
+            RollList = TmpList;
+        }
+
+        private List<List<int>> GetData(bool first)
+        {
+            int[] date = { DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day };
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load(PollAdress + string.Join("-", date) + "/1");
+
+            HtmlNode table = document.GetElementbyId(TableId).SelectSingleNode("//tbody");
+
+            List<List<int>> results = new List<List<int>>();
+
+            foreach (HtmlNode tr in table.SelectNodes("tr"))
+            {
+                foreach (HtmlNode td in tr.SelectNodes("td").Skip(1))
+                {
+                    if (td.SelectSingleNode("span") != null)
+                    {
+                        List<int> roll = new List<int>();
+                        
+                        foreach (HtmlNode span in td.SelectNodes("span"))
+                        {
+                            roll.Add(System.Int32.Parse(span.SelectSingleNode("span").InnerText));
+                        }
+
+                        results.Add(roll);
+                        
+                        if (first == true)
+                        {
+                            return results;
+                        }
+                    }
+                }
+            }
+
+            return results;
+        }
+    }
+}
